309e4fa81a956aaafb2417c80a1f235218fb14c2
tuminzee <t.sheth33@gmail.com>
2021-03-07 01:28:52 +0530

    Fix Image Store
    Add Cloudinary Id, Url Field in the Model.

--- controllers/data.controller.js
+++ controllers/data.controller.js
@@ -1,24 +1,20 @@
 var mongoose = require('mongoose');
 const Data = require('../models/data.model');
-const cloudinary = require('../utils/cloudinary');
 
 
 exports.createData = async (req, res) => {
-  const result = await cloudinary.uploader.upload(req.body.labReportFile);
-  console.log(result);
-
     const data = new Data({
       _id: mongoose.Types.ObjectId(),
-      // name: req.body.name,
-      // age: req.body.age,
-      // diseasesName: req.body.diseasesName,
-      // doctorsFeedback: req.body.doctorsFeedback,
-      // doctorsName: req.body.doctorsName,
-      // email: req.body.email,
-      // hospitalsName: req.body.hospitalsName,
-      labReportFileId: result.public_id,
-      labReportFileUrl: result.secure_url
-      // prescription: req.body.prescription
+      name: req.body.name,
+      age: req.body.age,
+      diseasesName: req.body.diseasesName,
+      doctorsFeedback: req.body.doctorsFeedback,
+      doctorsName: req.body.doctorsName,
+      email: req.body.email,
+      hospitalsName: req.body.hospitalsName,
+      labReportFileId: req.body.labReportFileId,
+      labReportFileUrl: req.body.labReportFileUrl,
+      prescription: req.body.prescription
     });
     return data
       .save()
@@ -26,7 +22,7 @@ exports.createData = async (req, res) => {
         return res.status(201).json({
           success: true,
           message: 'User Data created successfully',
-          data: { data: newData, result: result}
+          data: { data: newData}
         });
       })
       .catch((error) => {
--- models/data.model.js
+++ models/data.model.js
@@ -1,38 +1,38 @@
 const mongoose = require('mongoose');
 
 const DataSchema = new mongoose.Schema({
-    // name: {
-    //     type: String,
-    //     required: true
-    // },
-    // age: {
-    //     type: Number,
-    //     required: true
-    // },
-    // diseasesName: {
-    //     type: String,
-    //     required: true
-    // },
-    // doctorsFeedback: {
-    //     type: String,
-    //     required: true
-    // },
-    // doctorsName: {
-    //     type: String,
-    //     required: true
-    // },
-    // email: {
-    //     type: String,
-    //     required: true
-    // },
-    // hospitalsName: {
-    //     type: String,
-    //     required: true
-    // },
-    // prescription: {
-    //     type: String,
-    //     required: true
-    // },
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    diseasesName: {
+        type: String,
+        required: true
+    },
+    doctorsFeedback: {
+        type: String,
+        required: true
+    },
+    doctorsName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    hospitalsName: {
+        type: String,
+        required: true
+    },
+    prescription: {
+        type: String,
+        required: true
+    },
     labReportFileId: {
         type: String,
         required: true
@@ -41,14 +41,14 @@ const DataSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    // caseCloseDate: {
-    //     type: Date,
-    //     // required: true
-    // },
-    // caseOpenDate: {
-    //     type: Date,
-    //     // required: true
-    // },
+    caseCloseDate: {
+        type: Date,
+        // required: true
+    },
+    caseOpenDate: {
+        type: Date,
+        // required: true
+    },
 });
 
 
